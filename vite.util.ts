52fb7fe587d52d379ddf5d1a5d73fbe09d40f15a
xj <471256548@qq.com>
2022-09-08 09:52:40 +0800

    fix: fix vite config

--- vite.util.ts
+++ vite.util.ts
@@ -1,4 +1,4 @@
-import { GetModuleInfo } from 'rollup'
+import {GetModuleInfo} from 'rollup'
 export class SplitVendorChunkCache {
   public cache: Map<any, any>
   constructor() {
@@ -32,4 +32,25 @@ export function staticImportedByEntry(id: any, getModuleInfo: GetModuleInfo, cac
   )
   cache.set(id, someImporterIs)
   return someImporterIs
+}
+
+export function manualChunks(id: string, getModuleInfo: GetModuleInfo) {
+  const cache = new SplitVendorChunkCache()
+  const cssLangs = `\\.(css|less|sass|scss|styl|stylus|pcss|postcss)($|\\?)`
+  const cssLangRE = new RegExp(cssLangs)
+  const isCSSRequest = (request: string): boolean => cssLangRE.test(request)
+  // 分vendor包
+  if (
+      id.includes('node_modules') &&
+      !isCSSRequest(id) &&
+      staticImportedByEntry(id, getModuleInfo, cache.cache)
+  ) {
+    return 'vendor#' + id.toString().split('node_modules/')[1].split('/')[0].toString()
+  } else if (
+      // 分manifest包，解决chunk碎片问题
+      ((getModuleInfo(id)!.importers.length + getModuleInfo(id)!.dynamicImporters.length) > 1) &&
+      id.includes('src')
+  ) {
+    return 'manifest'
+  }
 }
