53566e5f7e2ac8cbd1907e44565e8c30fab28b6a
1 <1>
2022-09-09 11:10:51 +0800

    feat: request review

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,2 +1,18 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig, AxiosInstance, AxiosResponse } from 'axios'
+import { BASE_URL } from './const'
 
+const axiosRequestConfig: AxiosRequestConfig = {
+  baseURL: BASE_URL,
+  timeout: 10000
+}
+
+const request: AxiosInstance = axios.create(axiosRequestConfig)
+
+export function get<T = Record<string, any>, U = any>(url: string, params: T, config: AxiosRequestConfig = {}): Promise<AxiosResponse<U>> {
+  const requestConfig: AxiosRequestConfig = { ...config, data: params }
+  return request.get(url, requestConfig)
+}
+
+export function post<T = Record<string, any>, U = any>(url: string, data: T, config: AxiosRequestConfig = {}): Promise<AxiosResponse<U>> {
+  return request.post(url, data, config)
+}
