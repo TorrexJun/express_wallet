d953d13d70a29ce0cf44ec9992d94efbda705338
xiaojun <471256548@qq.com>
2022-09-16 17:02:43 +0800

    feat: import auto

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { viteVConsole } from "vite-plugin-vconsole"
 import { GetManualChunkApi } from 'rollup'
 import { manualChunks } from './vite.util'
 import * as path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 
 
@@ -21,7 +25,31 @@ export default ({ mode, command }: ConfigEnv): UserConfigExport  => {
           maxLogNumber: 1000,
           theme: 'light'
         }
-      })
+      }),
+      AutoImport({
+        // Auto import functions from Vue, e.g. ref, reactive, toRef...
+        imports: ['vue'],
+  
+        // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+        resolvers: [
+          // Auto import icon components
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+        ],
+      }),
+      Components({
+        resolvers: [
+          // Auto register icon components
+          IconsResolver({
+            enabledCollections: ['ep'],
+          }),
+        ],
+      }),
+  
+      Icons({
+        autoInstall: true,
+      }),
     ],
     build: {
       target: 'es2015',
