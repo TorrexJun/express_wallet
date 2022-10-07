1afcbdb3f5195928426957b9c6a1c5fc5e592d97
xiaojun <471256548@qq.com>
2022-10-07 11:06:13 +0800

    feat: img

--- src/view/home/homeIndex.vue
+++ src/view/home/homeIndex.vue
@@ -15,6 +15,7 @@ console.log(fn(1, 2, 4))
     <a href="https://vuejs.org/" target="_blank">
       <img src="../../assets/vue.svg" class="logo vue" alt="Vue logo" />
       <img src="../../assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="../../assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
