aa24ea766fa214cfabe26e4dad063ca9a65a871b
xiaojun <471256548@qq.com>
2022-10-13 10:21:58 +0800

    feat: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router'
-//
+
+const home = () => import('../view/home/homeIndex.vue')
+
 const Router = createRouter({
   history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
   routes: [
     {
       name: 'index',
       path: '/',
-      component: () => import('../view/home/homeIndex.vue'),
+      component: home,
     },
   ],
 })
