426bd7cb07a5582618357b7c15fb048fecdc4700
TorrexJun <31124807+TorrexJun@users.noreply.github.com>
2023-10-30 09:03:44 +0800

    Update layout.ts

--- src/mocks/layout.ts
+++ src/mocks/layout.ts
@@ -27,7 +27,7 @@ export const layouts: Slide[] = [
         top: 0,
         width: 310,
         height: 320,
-        viewBox: [211, 231],
+        viewBox: [212, 231],
         path: 'M 0 0 L 0 2010 L 200 200 Z',
         fill: '{{themeColor}}',
         fixedRatio: false,
